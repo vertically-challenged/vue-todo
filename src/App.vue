33db3c5d2a07c60631c19dd0debe69815a4859c0
vertically-challenged <50969768+vertically-challenged@users.noreply.github.com>
2022-07-06 17:55:32 +0300

    Vue project initialization

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
